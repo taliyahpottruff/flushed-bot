21dff68218889456652df8c2c66dc170eb938ba2
Trenton Pottruff <tpottruff@gmail.com>
2021-02-21 15:11:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flushed-bot
+ 
---
+++ flushed.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on("message", (message) => {
+    const simpleMessage = message.content.toLowerCase();
+
+    if (simpleMessage.startsWith('im') || simpleMessage.startsWith('i\'m')) {
+        message.react('😳');
+    }
+});
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
